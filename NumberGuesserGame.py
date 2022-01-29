00b7cad618016d2d085cd900e78cc7a2e306de4a
Ironblyat <77847032+Ironblat@users.noreply.github.com>
2022-01-29 14:55:48 +0100

    Update
    Some bug fixes and ne feature

--- NumberGuesserGame.py
+++ NumberGuesserGame.py
@@ -4,24 +4,39 @@ from random import randrange
 
 reset2 = True
 
-lang = input("Válassz nyelvet! / Choose Language! (M-Magyar/E-English): ")
+print("The Number Guesser Game v1.2      Created by: Ironblyat")
+
+while True:
+    lang = input("Válassz nyelvet! / Choose Language! (M-Magyar/E-English): ")
+    lang_invalid = False
+
+    if lang == 'M' or lang == 'E':
+        break
+    print("Nem jó/Invalid")
+
+
 
 if lang == 'M':
 
-    print('Ki kell találnod egy számot 1 és 100 kötül.')
+    print('Ki kell találnod egy számot 1 és 100 között. Írd be hogy "exit" a kilépéshez. Van 8 lehetőséged tippelni')
 
     while reset2:
         szam1 = int(randrange(1, 100))
-        tippek = 8
+        tippek = 7
 
-        for i in range(8):
-            szam2 = int(input('A tipped: '))
+        for i in range(7):
+            szam2 = input('A tipped: ')
+
+            if szam2 == "exit":
+                break
+            else:
+                szam2 = int(szam2)
 
             if szam1 == szam2:
                 print("Kiatáltad a számot")
                 break
             else:
-                print("nem találtad ki próbáld újra.")
+                print("nem találtad ki próbáld újra. Még ennyi tipped van: ", tippek)
 
 
                 if szam1 < szam2:
@@ -39,10 +54,10 @@ if lang == 'M':
         reset = input("Szeretnél még egy számot? (Y/N): ")
 
 
-        if reset == 'Y':
+        if reset == 'Y' or reset == 'y':
              reset2 = True
 
-        if reset == 'N':
+        if reset == 'N' or reset == 'n':
              reset2 = False
              print('Szia!!')
 
@@ -54,21 +69,26 @@ if lang == 'M':
             continue
 
 if lang == 'E':
-    print('You need to guess a number between 1 and 100')
+    print('You need to guess a number between 1 and 100. Type "exit" to exit the game. You have 8 guesses.')
 
     while reset2:
         szam1 = int(randrange(1, 100))
-        tippek = 8
+        tippek = 7
 
-        for i in range(8):
+        for i in range(7):
 
-            szam2 = int(input('your guess: '))
+            szam2 = input('your guess: ')
+
+            if szam2 == "exit":
+                break
+            else:
+                szam2 = int(szam2)
 
             if szam1 == szam2:
                 print("Hurray, you guessed the number.")
                 break
             else:
-                print("Try again.")
+                print("Try again. Number of tries left: ", tippek)
 
                 if szam1 < szam2:
                     print("Tipp: The number is lower")
@@ -83,10 +103,10 @@ if lang == 'E':
 
         reset = input("Do you want to play again? (Y/N): ")
 
-        if reset == 'Y':
+        if reset == 'Y' or reset2 == 'y':
             reset2 = True
 
-        if reset == 'N':
+        if reset == 'N' or reset == 'n':
             reset2 = False
             print('Goodbye!!')
 
@@ -95,4 +115,3 @@ if lang == 'E':
 
         else:
             continue
-
